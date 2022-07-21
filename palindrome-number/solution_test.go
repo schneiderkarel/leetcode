abd7cb0620cafc8fbb855d17009143df596b2223
schneiderkarel <74652261+schneiderkarel@users.noreply.github.com>
2022-07-21 09:02:06 +0200

    E 9. Palindrome Number (#2)
    * E 9. Palindrome Number
    
    * fixup! E 9. Palindrome Number
    
    Co-authored-by: ksch <karel.schneider@nextsum.com>

---
+++ palindrome-number/solution_test.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_isPalindrome(t *testing.T) {
+	type args struct {
+		x int
+	}
+	type exp struct {
+		res bool
+	}
+	tcs := []struct {
+		name string
+		args args
+		exp  exp
+	}{
+		{
+			name: "positive palindrome x",
+			args: args{
+				x: 1001,
+			},
+			exp: exp{
+				res: true,
+			},
+		},
+		{
+			name: "negative non-palindrome x",
+			args: args{
+				x: -1001,
+			},
+			exp: exp{
+				res: false,
+			},
+		},
+		{
+			name: "positive non-palindrome x",
+			args: args{
+				x: 100,
+			},
+			exp: exp{
+				res: false,
+			},
+		},
+		{
+			name: "invalid entry",
+			args: args{
+				x: int(math.Pow(-2, 31) - 1),
+			},
+			exp: exp{
+				res: false,
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.exp.res, isPalindrome(tc.args.x))
+		})
+	}
+}
+
+func Test_validEntry(t *testing.T) {
+	type args struct {
+		x int
+	}
+	type exp struct {
+		res bool
+	}
+	tcs := []struct {
+		name string
+		args args
+		exp  exp
+	}{
+		{
+			name: "valid positive x",
+			args: args{
+				x: int(math.Pow(2, 31) - 1),
+			},
+			exp: exp{
+				res: true,
+			},
+		},
+		{
+			name: "valid negative x",
+			args: args{
+				x: int(math.Pow(-2, 31)),
+			},
+			exp: exp{
+				res: true,
+			},
+		},
+		{
+			name: "invalid positive x",
+			args: args{
+				x: int(math.Pow(2, 31)),
+			},
+			exp: exp{
+				res: false,
+			},
+		},
+		{
+			name: "invalid negative x",
+			args: args{
+				x: int(math.Pow(-2, 31) - 1),
+			},
+			exp: exp{
+				res: false,
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.exp.res, validEntry(tc.args.x))
+		})
+	}
+}
