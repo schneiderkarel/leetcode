8793486bc52e2453b3451d3abf0bb33cd0187910
schneiderkarel <74652261+schneiderkarel@users.noreply.github.com>
2022-07-23 14:00:58 +0200

    E 20. Valid Parentheses (#8)
    Co-authored-by: ksch <karel.schneider@nextsum.com>

---
+++ valid-parentheses/solution.go
@@ -0,0 +1,55 @@
+package valid_parentheses
+
+import (
+	"math"
+)
+
+func isValid(str string) bool {
+	if !isEntryStringValid(str) {
+		return false
+	}
+
+	var (
+		parenthesesPairs = map[string]string{
+			")": "(",
+			"]": "[",
+			"}": "{",
+		}
+		parenthesesQueue []string
+	)
+
+	for _, char := range str {
+		switch string(char) {
+		case "(":
+			parenthesesQueue = append(parenthesesQueue, "(")
+		case ")", "]", "}":
+			if len(parenthesesQueue) == 0 {
+				return false
+			}
+
+			if parenthesesPairs[string(char)] != parenthesesQueue[len(parenthesesQueue)-1] {
+				return false
+			}
+
+			parenthesesQueue = append(parenthesesQueue[len(parenthesesQueue):], parenthesesQueue[:len(parenthesesQueue)-1]...)
+		case "[":
+			parenthesesQueue = append(parenthesesQueue, "[")
+		case "{":
+			parenthesesQueue = append(parenthesesQueue, "{")
+		}
+	}
+
+	if len(parenthesesQueue) != 0 {
+		return false
+	}
+
+	return true
+}
+
+func isEntryStringValid(str string) bool {
+	if len(str) < 1 || float64(len(str)) > math.Pow10(4) {
+		return false
+	}
+
+	return true
+}
