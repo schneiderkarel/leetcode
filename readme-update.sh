65b26a3885e491123a95ffbd6f56e0dbc299ae36
schneiderkarel <74652261+schneiderkarel@users.noreply.github.com>
2022-07-25 12:52:52 +0200

    Root README.md update script (#13)
    * Root README.md update script
    
    * fixup! Root README.md update script
    
    Co-authored-by: ksch <karel.schneider@nextsum.com>

---
+++ readme-update.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+easy_questions_counter=0
+medium_questions_counter=0
+hard_questions_counter=0
+
+for entry in `pwd`/*/
+do
+  file=`ls $entry*.md`
+
+  if grep -q 'Easy' "$file"; then
+    ((easy_questions_counter = easy_questions_counter + 1))
+  elif grep -q 'Medium' "$file"; then
+     ((medium_questions_counter = medium_questions_counter + 1))
+  elif grep -q 'Hard' "$file"; then
+      ((hard_questions_counter = hard_questions_counter + 1))
+  fi
+done
+
+cat > README.md  << EOM
+# Leetcode
+
+Repository containing *[Leetcode](https://www.leetcode.com)* questions solutions.
+
+Each directory contains one *[Leetcode](https://www.leetcode.com)* question. Inside these directories are **three** files.
+
+1. \`README.md\` - contains the description of the question.
+2. \`solution.go\` - contains the functionality to solve the problem.
+3. \`solution_test.go\` - contains unit tests to validate the code inside \`solution.go\`.
+
+### Amount of Questions:
+
+    Easy: $easy_questions_counter
+    Medium: $medium_questions_counter
+    Hard: $hard_questions_counter
+EOM
